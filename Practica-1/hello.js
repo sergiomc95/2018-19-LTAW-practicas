756535c9f39dce6211fc39bf1218fc3b2ae7d2ad
Sergio Martin Corchon <sergiomc@beta26.aulas.gsyc.urjc.es>
2019-01-29 12:29:15 +0100

    Commit_p1

---
+++ Practica-1/hello.js
@@ -0,0 +1,4 @@
+//-- mi primer programa en node
+
+console.log("Hola cómo vas");
+proccess.stdout.write("otra forma de imprimir\n\n");
---
+++ Practica-1/wiki/server.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+console.log("Arrancando servidor...")
+
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hello World!');
+  console.log("Peticion atendida")
+}).listen(8080);
