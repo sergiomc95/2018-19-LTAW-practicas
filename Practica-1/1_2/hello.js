f5caa9342800f12b3cafbb50d74e2ee355739943
Sergio Martin Corchon <sergiomc@alpha19.aulas.gsyc.urjc.es>
2019-04-09 16:46:22 +0200

    Inicio la practica 2.

---
+++ Practica-1/1_2/hello.js
@@ -0,0 +1,4 @@
+//-- mi primer programa en node
+
+console.log("Hola cómo vas");
+process.stdout.write("otra forma de imprimir\n\n");
--- Practica-1/hello.js
+++
@@ -1,4 +0,0 @@
-//-- mi primer programa en node
-
-console.log("Hola cómo vas");
-proccess.stdout.write("otra forma de imprimir\n\n");
