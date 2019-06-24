760af4f7018d7c449135c59c131a3d7387b34cf8
sergiomc95 <sergiomartinc95@gmail.com>
2019-06-24 23:24:23 +0200

    finalizo actividades guiadas practica 2

---
+++ Practica-2/2_1/mi_tienda/mi_tienda/views.py
@@ -0,0 +1,42 @@
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Template, Context
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'main.html', {'user':'sergio'})
+
+
+def mi_funcion(request):
+	html = "Hola! Mi primera UrL!!"
+	return HttpResponse(html)
+
+def mi_producto(request, param):
+	numero = int(param)
+	html = "Acceso a producto: %i" % numero;
+	return HttpResponse(html)
+
+
+PLANTILLA = """
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Saludo</title>
+  </head>
+  <body>
+    <p>Bienvenido a mi tienda, {{user}}</p>
+  </body>
+</html>
+"""
+
+def saludo(request):
+    # --Procesar la plantilla
+    t = Template(PLANTILLA)
+
+    # -- Crear el contexto: Nombre de usuario real
+    c = Context({'user':'Epic Saxo guy'})
+
+    # -- Obtener la pagina html final
+    html = t.render(c)
+    return HttpResponse(html)
